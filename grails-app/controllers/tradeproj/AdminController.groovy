769055a287929f7d5c9e5570c6e53733191b9c30
bkishore <braj.kishore@neustar.biz>
2018-01-18 23:49:05 +0530

    refacoring some minor changes

--- grails-app/controllers/tradeproj/AdminController.groovy
+++ grails-app/controllers/tradeproj/AdminController.groovy
@@ -8,28 +8,15 @@ import org.apache.poi.ss.usermodel.WorkbookFactory
 class AdminController {
     def exportService
     def adminService
+    //It is a placeholder for the file as there is no unique identity to the file.
+    //will be changed at a later point of time
     static Map map = new HashMap()
 
     def index() {}
 
     def downLoadTradeExcel = {
-
-        /**
-         *
-         * <th>trade Date</th>
-         <th>exchange</th>
-         <th>type</th>
-         <th>qty</th>
-         <th>rate</th>
-         <th>rate</th>
-         <th>order</th>
-         <th>trade</th>
-         <th>comment</th>
-         <th>avgPrice</th>
-         */
-
         List fields=['tradeDate', 'exchange','type','qty','rate', 'orderNo', 'tradeNo','comment', 'avgPrice']
-        //Map labels=['tradeDate', 'exchange','type','qty','rate', 'order', 'trade','comment', 'avgPrice']
+
         List<Trade> data= map.get("map")
         response.setHeader("Content-disposition", "attachment; filename=TestReport.csv")
         exportService.export('csv', response.outputStream, data, fields, null, new HashMap(), new HashMap())
@@ -40,11 +27,8 @@ class AdminController {
             def file = request.getFile('file')
             List<Trade> tradeList = adminService.processTradeData(file)
             map.put("map", tradeList)
-            //render(view: "trade",  model: [tradeList:tradeList])
             redirect(action: "create")
             return false
-            //redirect(action: "edit")
-            //return false
         } catch (Exception e) {
             e.printStackTrace()
             println("exiting")
--- grails-app/services/tradeproj/AdminService.groovy
+++ grails-app/services/tradeproj/AdminService.groovy
@@ -27,76 +27,80 @@ class AdminService {
     def processTradeData(def file) {
         try {
             Workbook workbook = WorkbookFactory.create(file.getInputStream())
-            List tradeList = excelImportService.columns(workbook, CONFIG_BOOK_COLUMN_MAP)
+            List tradeListFromFile = excelImportService.columns(workbook, CONFIG_BOOK_COLUMN_MAP)
 
-            Map columnMap1 = tradeList.remove(0)
+            Map columnMap1 = tradeListFromFile.remove(0)
 
             println("\n-------------------------------\n")
             println("\ncolumnMap=$columnMap1 \n")
             List<Trade> tradeObjList = new ArrayList<>()
-            tradeList.each {
+            tradeListFromFile.each {
                 println(it)
                 tradeObjList.add(new Trade(it))
             }
             println("\n-------------------------------\n")
             println(tradeObjList)
             println("\n-------------------------------\n")
-            def tradeObjectGroupByDate = tradeObjList.groupBy {it.tradeDate}
-            List<Trade> eqTradeObjList = new ArrayList<>()
-            tradeObjectGroupByDate.each {tDate, tObjectList ->
-                List<Trade> tempList = tObjectList
-                if(tObjectList.size()>1){
-                    tempList = getEquivalentTradeForTheDay(tObjectList)
-                }
-                eqTradeObjList.addAll(tempList)
-            }
-            println("$tradeObjectGroupByDate\n--------------GroupByDate above-----------------\n")
-            //generateReport(tradeList)
-            generateReport(eqTradeObjList)
+            processTrades(tradeObjList)
         } catch (Exception e) {
             e.printStackTrace()
             println("exiting")
         }
     }
 
-    List<Trade> getEquivalentTradeForTheDay(List<Trade> tradeObjList){
+    private void processTrades(ArrayList<Trade> tradeObjList) {
+        def tradeObjectGroupByDate = tradeObjList.groupBy { it.tradeDate }
+        List<Trade> eqTradeObjList = new ArrayList<>()
+        tradeObjectGroupByDate.each { tDate, tObjectList ->
+            List<Trade> tempList = tObjectList
+            if (tObjectList.size() > 1) {
+                tempList = getEquivalentTradeForTheDay(tObjectList)
+            }
+            eqTradeObjList.addAll(tempList)
+        }
+        println("$tradeObjectGroupByDate\n--------------GroupByDate above-----------------\n")
+        //generateReport(tradeList)
+        generateReport(eqTradeObjList)
+    }
+
+    private List<Trade> getEquivalentTradeForTheDay(List<Trade> tradeObjList) {
         int buyQuantity = tradeObjList.sum {
-            if(it.type == Trade.TRADETYPE.BUY.toString()){
+            if (it.type == Trade.TRADETYPE.BUY.toString()) {
                 it.qty
-            }else{
+            } else {
                 0
             }
         }
 
         int sellQuantity = tradeObjList.sum {
-            if(it.type == Trade.TRADETYPE.BUY.toString()){
+            if (it.type == Trade.TRADETYPE.BUY.toString()) {
                 0
-            }else{
+            } else {
                 it.qty
             }
         }
         println(buyQuantity)
         println(sellQuantity)
 
-        if(buyQuantity >sellQuantity){
-            return createList(buyQuantity-sellQuantity, 'B', tradeObjList)
-        }else if(sellQuantity>buyQuantity){
+        if (buyQuantity > sellQuantity) {
+            return createList(buyQuantity - sellQuantity, 'B', tradeObjList)
+        } else if (sellQuantity > buyQuantity) {
             return createList(sellQuantity - buyQuantity, 'S', tradeObjList)
-        }else{
+        } else {
             return new ArrayList<Trade>()
         }
     }
 
-    List<Trade> createList(int quantityToCollect, String type,  List<Trade> tradeObjList){
+    private List<Trade> createList(int quantityToCollect, String type, List<Trade> tradeObjList) {
         List<Trade> resultList = new ArrayList<>()
-        for(int i=tradeObjList.size()-1; i>=0 && quantityToCollect>0; i--){
+        for (int i = tradeObjList.size() - 1; i >= 0 && quantityToCollect > 0; i--) {
             Trade t = tradeObjList.get(i)
 
             if (t.type == type) {
-                if(quantityToCollect>t.qty){
+                if (quantityToCollect > t.qty) {
 
                     quantityToCollect = quantityToCollect - t.qty
-                }else{
+                } else {
                     t.qty = quantityToCollect
                     quantityToCollect = 0
                 }
@@ -106,13 +110,13 @@ class AdminService {
         return resultList
     }
 
-    def generateReport(List<Trade> tradeObjList) {
+    private def generateReport(List<Trade> tradeObjList) {
         LinkedList<Trade> buyTradeList = new LinkedList<>()
         int actualQuantity = 0
         double totalPrice = 0
         def curTradeDate = null
         tradeObjList.each {
-            if(curTradeDate != null && curTradeDate != it.tradeDate ){
+            if (curTradeDate != null && curTradeDate != it.tradeDate) {
 
             }
             if (it.type == Trade.TRADETYPE.BUY.toString()) {
@@ -122,7 +126,7 @@ class AdminService {
             } else {
                 int remainingSellQty = it.qty
                 Iterator itr = buyTradeList.iterator()
-                while (buyTradeList.size()>0 && remainingSellQty > 0) {
+                while (buyTradeList.size() > 0 && remainingSellQty > 0) {
                     Trade t = buyTradeList.get(0)
                     if (t.qty <= remainingSellQty) {
                         actualQuantity = actualQuantity - t.qty
@@ -136,13 +140,13 @@ class AdminService {
                         totalPrice = totalPrice - (remainingSellQty * t.rate)
 
                         t.setQty(t.qty - remainingSellQty)
-                        buyTradeList.set(0,t)
+                        buyTradeList.set(0, t)
                         remainingSellQty = remainingSellQty - remainingSellQty
                         println("$actualQuantity $remainingSellQty $totalPrice")
                     }
                 }
             }
-            it.avgPrice = totalPrice / actualQuantity
+            it.avgPrice = actualQuantity != 0 ? totalPrice / actualQuantity : 0
             it.comment = "net qty=$actualQuantity, net price=$totalPrice, avg price=${it.avgPrice}"
             println("avg=${it.avgPrice} actqty = ${it.netQuantity}")
         }
